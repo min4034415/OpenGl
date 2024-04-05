d1b5b8124c6da0b332b6735b9e4450c4ba00a330
min4034415 <min4034415@gmail.com>
2024-04-05 11:01:06 +0900

    깃허브 너무 어려워요

--- testGl/main.c
+++ testGl/main.c
@@ -13,6 +13,10 @@
 #include <GL/gl.h>
 #include <GL/glut.h>
 #endif
+//이거 아니면 윈도우 생성이 안됨
+//int main(int argc, char** argv) {
+//    glutInit(&argc, argv); // Initialize GLUT
+
 
 void MyDisplay() {
     glClear(GL_COLOR_BUFFER_BIT);
